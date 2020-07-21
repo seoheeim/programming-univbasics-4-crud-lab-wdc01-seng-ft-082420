64f60e91c925557207ba259175ae8f5283315b55
Seohee  Im <seoheeim@vt.edu>
2020-07-21 17:39:58 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,52 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  the_array = ["a", "b", "c", "d"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+  array << "arrays!"
+  p array 
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  array.unshift("wow")
+  p array
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  new_array = array.pop
+  p new_array
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  new_array = array.shift
+  p new_array
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array =["wow", "I", "am", "really", "learning", "arrays!"]
+  p array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  p array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  p array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array[4] = "totally"
+p array[4]
 end
