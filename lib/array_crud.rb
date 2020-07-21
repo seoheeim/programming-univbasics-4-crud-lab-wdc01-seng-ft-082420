dff0cca6c51b35ec1a4baab306bbeb5d236190eb
Seohee  Im <seoheeim@vt.edu>
2020-07-21 17:17:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,52 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  the_array = ["a", "b", "c", "d"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["a", "b", "c"]
+  array << element
+  p array 
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["a", "b", "c"]
+  array.unshift(element)
+  p array
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["a", "b", "c"]
+  new_array = array.pop
+  p new_array
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["a", "b", "c"]
+  new_array = array.shift
+  p new_array
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array =["a", "b", "c"]
+  index_number = 0 
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["a", "b"]
+  p array[1]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["a", "b"]
+  p array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+array = ["a", "b", "c"]
+element = array[2]
+p array 
 end
